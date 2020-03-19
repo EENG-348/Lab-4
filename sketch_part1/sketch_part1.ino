85383fec91204f7bc69183d66e91734c1b8baf2e
danesg29 <43322439+danesg29@users.noreply.github.com>
2020-03-19 12:37:41 -0700

    Add files via upload

---
+++ sketch_part1/sketch_part1.ino
@@ -0,0 +1,100 @@
+// EENG 348: Digital Systems - Lab 4
+// Daniel Esguerra and Mawuli Akpalu
+
+/********* SKETCH_PART1 ***********/
+
+#include "concurreny.h" // code provided by Prof. Manohar
+
+struct process_state
+{
+    unsigned int sp;            // stack pointer
+    struct process_state* next; // pointer to next process
+};
+
+void p1();
+void p2();
+
+void setup()
+{
+    // Set the baud rate for output
+    Serial.begin(9600);
+
+    // Set the current process to NULL
+    current_process = NULL;
+
+    // Return 0 on failure to create Process 1
+    if (process_create(p1, 64) < 0)
+        return 0;
+
+    // Return 0 on failure to create Process 2
+    if (process_create(p1, 64) < 0)
+        return 0;
+}
+
+void loop()
+{
+    // Begin executing processes
+    process_start();
+
+    // Stall after all processes have executed
+    while (1);
+}
+
+int process_create(void (*f)(void), int n)
+{
+    process_t* new_process;
+
+    // Allocate space for a new process struct
+    new_process = malloc(sizeof(process_t));
+
+    // Return error on failure to allocate space
+    if (new_process == NULL)
+    {
+        Serial.println("ERROR 0x01: Failure to allocate memory");
+        return -1;
+    }
+
+    // Initialize new process f() with an n-byte stack
+    process_init(f, n);
+
+    // Return 0 on success
+    return 0;
+}
+
+void process_start(void)
+{
+    // Initialize concurrent data structures
+
+    // Start the execution of the next process
+    process_begin();
+}
+
+unsigned int process_select(unsigned int cursp)
+{
+    process_t* next_process;
+
+    // Assign the pointer to the next process
+    next_process = current_process->next;
+
+    // Assign the next process to the current process
+    current_process = next_process;                         /** DOES THIS NEED TO BE UPDATED??? **/
+
+    // Return 0 if no process is ready to execute
+    if (next_process == NULL)
+        return 0;
+
+    // Return the stack pointer for the next process
+    return next_process->sp;
+}
+
+// Process 1
+void p1(void)
+{
+    return;
+}
+
+// Process 2
+void p2(void)
+{
+    return;
+}
